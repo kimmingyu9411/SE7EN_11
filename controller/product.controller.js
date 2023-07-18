94ae9a5f006befebff6e73a8ff14e25ad5a421ca
baechanyong1 <110149885+baechanyong1@users.noreply.github.com>
2023-07-18 10:16:19 +0900

    뼈대 작성 2

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,7 +1,15 @@
-import {ProductService} from '../service/productService.js'
+import { ProductService } from "../service/productService.js";
 
-export class ProductController{
-    constructor(){
-        this.productService = new ProductService();
-    }
+export class ProductController {
+  constructor() {
+    this.productService = new ProductService();
+  }
+
+  getProduct = (req, res, next) => {};
+
+  createProduct = (req, res, next) => {};
+
+  updateProduct = (req, res, next) => {};
+
+  deleteProduct = (req, res, next) => {};
 }
