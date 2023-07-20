bb613f9a8eec88ed2b74a7b2dc6416627052ad1d
mingyu kim9411 <awer159@daum.net>
2023-07-20 11:06:28 +0900

    점포 생성/조회/특정 조회/업데이트 완료

--- controller/store.controller.js
+++ controller/store.controller.js
@@ -17,7 +17,6 @@ class StoreController {
 
   getOneStore = async (req, res, next) => {
     const storeId = req.params;
-
     const getOne = await this.storeService.getOneStore(storeId);
     if (getOne.status === 400) {
       res.status(400).json({ message: getOne.errorMessage });
@@ -32,18 +31,22 @@ class StoreController {
 
     const create = await this.storeService.createStore(user, name, address);
     if (create.status === 400) {
-      res.status(400).json({ message: getOne.errorMessage });
+      res.status(400).json({ message: create.errorMessage });
     } else {
       res.status(200).json({ message: "가게 생성에 성공했습니다." });
     }
   };
 
   updateStore = async (req, res, next) => {
-    const storeId = req.params;
-    const user = res.locals.user;
-    const { name } = req.body;
-
-    const update = await this.storeService.updateStore(storeId, user, name);
+    const { storeId } = req.params;
+    const {id} = res.locals.user;
+    const { name, address } = req.body;
+    const update = await this.storeService.updateStore(
+      storeId,
+      id,
+      name,
+      address
+    );
     if (update.status === 400) {
       res.status(400).json({
         message: update.errorMessage,
