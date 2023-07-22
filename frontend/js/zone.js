a84fd1091426124bb448827584a85895eece3172
Jang Sihun <chocoj1123@naver.com>
2023-07-23 01:16:51 +0900

    프론트단 다시 제대로 연결(수정중) + 민규님작업

--- frontend/js/zone.js
+++ frontend/js/zone.js
@@ -5,114 +5,88 @@ function zoneCategory() {
   const temp = document.createElement("ul");
   temp.innerHTML = `
     <li class="product">
-      <div id="delivery">
-      <div class="deliveryimg">
-        <img src="./img/배달이미지.png" />
-      </div>
-      </div>
-    </li>
-    <li class="product">
       <strong>전체보기</strong>
-      <div class="logoimg">
+      <div class="produtimg">
         <img src="./img/로고.png" />
       </div>
     </li>
-    <li class="product">
-      <div id="sneck" onclick ="location.href='detailzone.html?category=과자&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=과자&id=${storeId}'">
       <strong>과자</strong>
-      <div class="sneckimg">
+      <div class="produtimg">
         <img src="./img/과자.png" />
       </div>
-    </div>
     </li>
-    <li class="product">
-      <div id="beverage" onclick ="location.href='detailzone.html?category=음료&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=음료&id=${storeId}'">
       <strong>음료</strong>
-      <div class="beverageimg">
+      <div class="produtimg">
         <img src="./img/음료.png" />
-      </div>
-    </div>
+      </div>   
     </li>
-    <li class="product">
-      <div id="item" onclick ="location.href='detailzone.html?category=생필품&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=생필품&id=${storeId}'">     
       <strong>생필품</strong>
-      <div class="itemimg">
+      <div class="produtimg">
         <img src="./img/생필품.png" />
-      </div>
-    </div>
+      </div>    
     </li>
-    <li class="product">
-      <div id="lunchBox" onclick ="location.href='detailzone.html?category=도시락&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=도시락&id=${storeId}'">
       <strong>도시락</strong>
-      <div class="lunchBoximg">
+      <div class="produtimg">
         <img src="./img/도시락.png" />
-      </div>
-    </div>
+      </div>   
     </li>
-    <li class="product">
-      <div id="medical" onclick ="location.href='detailzone.html?category=의약품&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=의약품&id=${storeId}'">
       <strong>의약품</strong>
-      <div class="medicalimg">
+      <div class="produtimg">
         <img src="./img/의약품.png" />
       </div>
-    </div>
     </li>
-    <li class="product">
-      <div id="ramen" onclick ="location.href='detailzone.html?category=라면&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=라면&id=${storeId}'">
       <strong>라면</strong>
-      <div class="ramenimg">
+      <div class="produtimg">
         <img src="./img/라면.png" />
       </div>
-    </div>
     </li>
-    <li class="product">
-      <div id="sandwich" onclick ="location.href='detailzone.html?category=샌드위치&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=샌드위치&id=${storeId}'">    
       <strong>샌드위치</strong>
-      <div class="sandwichimg">
+      <div class="produtimg">
         <img src="./img/샌드위치.png" />
       </div>
-    </div>
     </li>
-    <li class="product">
-      <div id="braad" onclick ="location.href='detailzone.html?category=빵&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=빵&id=${storeId}'">
       <strong>빵</strong>
-      <div class="braadimg">
+      <div class="produtimg">
         <img src="./img/빵.png" />
       </div>
     </li>
-    <li class="product">
-      <div id="ice" onclick ="location.href='detailzone.html?category=빙과류&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=빙과류&id=${storeId}'">
       <strong>빙과류</strong>
-      <div class="iceimg">
+      <div class="produtimg">
         <img src="./img/빙과류.png" />
-      </div>
-    </div>
+      </div>  
     </li>
-    <li class="product">
-      <div id="drink" onclick ="location.href='detailzone.html?category=주류&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=주류&id=${storeId}'">
       <strong>주류</strong>
-      <div class="drinkimg">
+      <div class="produtimg">
         <img src="./img/주류.png" />
-      </div>
-    </div>
+      </div>   
     </li>
-    <li class="product">
-      <div id="iceProduct" onclick ="location.href='detailzone.html?category=냉동식품&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=냉동식품&id=${storeId}'">
       <strong>냉동식품</strong>
-      <div class="iceProductimg">
+      <div class="produtimg">
         <img src="./img/냉동식품.png" />
-      </div>
-    </div>
+      </div>   
     </li>
-    <li class="product">
-      <div id="cignature" onclick ="location.href='detailzone.html?category=시그니처&id=${storeId}'">
+    <li class="product" onclick ="location.href='detailzone.html?category=시그니처&id=${storeId}'">
       <strong>시그니처</strong>
-      <div class="cignatureimg">
+      <div class="produtimg">
         <img src="./img/편마카세.png" />
-      </div>
-    </div>
+      </div>  
     </li>
   `;
   document.querySelector(".products").append(temp);
 }
 zoneCategory();
+
+const nameData = storeId;
+
+document.querySelector(".storename").textContent = nameData;
--- repository/store.repository.js
+++ repository/store.repository.js
@@ -1,9 +1,19 @@
 const Product = require("../database/model/product");
 const Store = require("../database/model/store");
 const User = require("../database/model/user");
-const sq = require('../database/db.js').sequelize;
+const sq = require("../database/db.js").sequelize;
 
 class StoreRepository {
+  async getStoreName(storeId) {
+    try {
+      const store = await Store.findByPk(storeId);
+      return store ? store.name : null;
+    } catch (error) {
+      console.error("상점 이름 조회 중 오류:", error);
+      return null;
+    }
+  }
+
   async createStore(user, name, address) {
     const t = await sq.transaction();
     try {
@@ -15,15 +25,18 @@ class StoreRepository {
           errorMessage: "이미 사용 중인 점포명입니다.",
         };
       }
-      const store = await Store.create({
-        name,
-        address,
-        userId: user.id,
-      },{transaction:t});
-      
-      const owner = await User.findByPk(user.id,{transaction:t});
+      const store = await Store.create(
+        {
+          name,
+          address,
+          userId: user.id,
+        },
+        { transaction: t }
+      );
 
-      await owner.setStore(store,{transaction:t});
+      const owner = await User.findByPk(user.id, { transaction: t });
+
+      await owner.setStore(store, { transaction: t });
       await t.commit();
 
       return store;
@@ -39,12 +52,12 @@ class StoreRepository {
 
   async getOneStore(storeId) {
     try {
-      const store = await Store.findByPk(storeId,{
-        include:{
-          model:Product,
-          as:'ProductList',
-          attributes:['name','category','price']
-        }
+      const store = await Store.findByPk(storeId, {
+        include: {
+          model: Product,
+          as: "ProductList",
+          attributes: ["name", "category", "price"],
+        },
       });
 
       if (!store) {
@@ -80,7 +93,9 @@ class StoreRepository {
 
   async updateStore(storeId, id, updateValues) {
     try {
-      const existingStore = await Store.findOne({ where: {name:updateValues.name} });
+      const existingStore = await Store.findOne({
+        where: { name: updateValues.name },
+      });
       const store = await Store.update(updateValues, {
         where: { id: storeId, userId: id },
       });
@@ -121,7 +136,7 @@ class StoreRepository {
 
       await store.destroy();
 
-      return { message:"점포 삭제가 완료되었습니다."};
+      return { message: "점포 삭제가 완료되었습니다." };
     } catch (error) {
       console.error("점포 삭제 중 오류:", error);
       return {
